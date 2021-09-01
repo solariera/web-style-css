1d71222a1fef21d124f13ca7b69ee3e1a7b18cd4
Solariera <alwaysrusting@gmail.com>
2021-09-02 00:00:07 +0900

    000028 [change] fontSizeUnit: unit -> fontUnit

--- src/text/fontSizeStyle/index.ts
+++ src/text/fontSizeStyle/index.ts
@@ -3,15 +3,15 @@ import { css } from '../../css';
 type FontSizeType = {
   fontWeight?: number;
   fontSize?: number;
-  unit?: string;
+  fontUnit?: string;
 };
 
 export const fontSizeStyle = (props: FontSizeType) => {
-  const { fontWeight = 400, fontSize = 0.7, unit = 'px' } = props;
+  const { fontWeight = 400, fontSize = 0.7, fontUnit = 'rem' } = props;
 
   const styleString = css`
     font-weight: ${fontWeight};
-    font-size: ${fontSize + unit};
+    font-size: ${fontSize + fontUnit};
   `;
 
   return styleString;

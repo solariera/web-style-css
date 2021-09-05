f15a71284e16fd8b4bd4bf0a43827dc345b5a21e
Solariera <alwaysrusting@gmail.com>
2021-09-05 22:04:49 +0900

    000036 [fix] bug fix

--- src/text/fontSizeStyle/index.ts
+++ src/text/fontSizeStyle/index.ts
@@ -1,16 +1,14 @@
 import { css } from '../../css';
 
 type FontSizeType = {
-  fontWeight?: number;
   fontSize?: number;
   fontUnit?: string;
 };
 
 export const fontSizeStyle = (props: FontSizeType) => {
-  const { fontWeight = 400, fontSize = 0.7, fontUnit = 'rem' } = props;
+  const { fontSize = 0.7, fontUnit = 'rem' } = props;
 
   const styleString = css`
-    font-weight: ${fontWeight};
     font-size: ${fontSize + fontUnit};
   `;
 
